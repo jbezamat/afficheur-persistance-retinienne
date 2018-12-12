fba1ec4bcd9968450af3be725b1a7743101ec2de
ZdTmasar <thomas.saliba@enseirb-matmeca.fr>
2018-12-12 10:28:07 +0100

    main upgraded

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 //#include <stdlib.h>
-//#include "USART.h"
+#include "USART.c"
+#include "hall_sensor.c"
+#include "SPI.c"
+#include "watch.c"
 
 #define FOSC F_CPU// Clock Speed
 #define BAUD 38400
 #define MYUBRR FOSC/8/BAUD-1
 
-int hour = 0;
-int minute = 0;
+//int hour = 0;
+//int minute = 0;
 
 float speed = 0;
 int mode = 0;
@@ -32,15 +35,15 @@ ISR(TIMER1_COMPA_vect)
     }
 }
 
-void USART_Init(unsigned int ubbr)
-{
-    UBRR0H = (unsigned char)(ubbr>>8); //Baudrate at 38400
-    UBRR0L = (unsigned char)(ubbr);
+// void USART_Init(unsigned int ubbr)
+// {
+//     UBRR0H = (unsigned char)(ubbr>>8); //Baudrate at 38400
+//     UBRR0L = (unsigned char)(ubbr);
 
-    UCSR0A = (_BV(U2X0)); //double speed
-    UCSR0B = (_BV(RXEN0)) | (_BV(TXEN0)) | (_BV(RXCIE0)); //enable RX and Tx and interrupt on reception
-    UCSR0C = (3 << (UCSZ00));                             //8bit char
-}
+//     UCSR0A = (_BV(U2X0)); //double speed
+//     UCSR0B = (_BV(RXEN0)) | (_BV(TXEN0)) | (_BV(RXCIE0)); //enable RX and Tx and interrupt on reception
+//     UCSR0C = (3 << (UCSZ00));                             //8bit char
+// }
 
 
 void Init_Interrupt()
@@ -48,109 +51,110 @@ void Init_Interrupt()
     sei();
 }
 
-void SPI_MasterInit()
-{    
-    /* Set MOSI and SCK output, all others input */
-    DDRB |= (0 << DDB0) | (1 << DDB2) | (1 << DDB1) ;
-    /* Enable SPI, Master, TODO: set clock rate fck/4 */
-    SPCR |= (1 << SPE) | (1 << MSTR);
-    /*Set PE4(OE), PE5 (LE)*/
-    DDRE |= (1 << DDE4) | (1 << DDE5);
+// void SPI_MasterInit()
+// {    
+//     /* Set MOSI and SCK output, all others input */
+//     DDRB |= (0 << DDB0) | (1 << DDB2) | (1 << DDB1) ;
+//     /* Enable SPI, Master, TODO: set clock rate fck/4 */
+//     SPCR |= (1 << SPE) | (1 << MSTR);
+//     /*Set PE4(OE), PE5 (LE)*/
+//     DDRE |= (1 << DDE4) | (1 << DDE5);
 
-    PORTB &= ~(1<<PB0);
-    // PORTE = PORTE | _BV(PE4) | _BV(PE5);
-}
+//     PORTB &= ~(1<<PB0);
+//     PORTE &= ~(1 << PORTE4); // OE
+//     // PORTE = PORTE | _BV(PE4) | _BV(PE5);
+// }
 
-void Init_Watch() 
-{
-    //Set prescaler to 1024
-    TCCR1B |= _BV(CS12) |_BV(CS10);
+// void Init_Watch() 
+// {
+//     //Set prescaler to 1024
+//     TCCR1B |= _BV(CS12) |_BV(CS10);
 
-    //Set CTC mode
-    TCCR1B |= _BV(WGM12);
+//     //Set CTC mode
+//     TCCR1B |= _BV(WGM12);
 
-    //Set overflow to 12695
-    OCR1AH = 0x31;
-    OCR1AL = 0x97;
+//     //Set overflow to 12695
+//     OCR1AH = 0x31;
+//     OCR1AL = 0x97;
 
-    //Set interrupt on compare match
-    TIMSK |= _BV(OCIE1A);
-}
+//     //Set interrupt on compare match
+//     TIMSK |= _BV(OCIE1A);
+// }
 
-void USART_Transmit(unsigned char data)
-{
-    while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = data;
-    while ((UCSR0A & (1 << TXC0)) == 0x00);
-}
+// void USART_Transmit(unsigned char data)
+// {
+//     while (!(UCSR0A & (1 << UDRE0)));
+//     UDR0 = data;
+//     while ((UCSR0A & (1 << TXC0)) == 0x00);
+// }
 
-void USART_puts(unsigned char *mot){
+// void USART_puts(unsigned char *mot){
 
-    //sprintf(buffer, "x=%u\r\n", TCNT1);
-    int k = 0;
-    while(mot[k] != '\0'){
-        USART_Transmit(mot[k]);
-        k++;
-    } 
-}
+//     //sprintf(buffer, "x=%u\r\n", TCNT1);
+//     int k = 0;
+//     while(mot[k] != '\0'){
+//         USART_Transmit(mot[k]);
+//         k++;
+//     } 
+// }
 
-void SPI_MasterTransmit(char cData, char cData2)
-{
-    /* Start transmission */
-    
-    PORTE &= ~(1 << PORTE4); // OE
+// void SPI_MasterTransmit(char cData, char cData2)
+// {
+//     /* Start transmission */
 
-    PORTB &= ~(1<<PB0);
-    SPDR = cData;
+//     PORTE &= ~(1 << PORTE4); // OE
 
-    /* Wait for transmission complete */
-    while (!(SPSR & (1 << SPIF)));
-    /* Start transmission */
-    PORTB |= (1<<PB0);
+//     PORTB &= ~(1<<PB0);
+//     SPDR = cData;
 
-    PORTB &= ~(1<<PB0);
-    SPDR = cData2;
-    /* Wait for transmission complete */
-    while (!(SPSR & (1 << SPIF)));
+//     /* Wait for transmission complete */
+//     while (!(SPSR & (1 << SPIF)));
+//     /* Start transmission */
+//     PORTB |= (1<<PB0);
+//     _delay_us(20);
+//     PORTB &= ~(1<<PB0);
+//     SPDR = cData2;
+//     /* Wait for transmission complete */
+//     while (!(SPSR & (1 << SPIF)));
 
-    PORTB |= (1<<PB0);
+//     PORTB |= (1<<PB0);
 
-    /* Latch and Output enable terminal*/
-    PORTE |= (1 << PORTE5); // latch
-    PORTE &= ~(1 << PORTE5);
+//     /* Latch and Output enable terminal*/
+//     PORTE |= (1 << PORTE5); // latch
+//     PORTE &= ~(1 << PORTE5);
 
-    PORTE |= (1 << PORTE4);
-    PORTE &= ~(1 << PORTE4); // OE
+//     PORTE |= (1 << PORTE4);
+//     PORTE &= ~(1 << PORTE4); // OE
 
-}
+// }
 
 
-int atoi(char* s, int l)
-{
-    int val = 0;
+// int atoi(char* s, int l)
+// {
+//     int val = 0;
 
-    int i = 1;
-    while(i <= l) {
-        if(s[l-i] < '0' || s[l-i] > '9')
-            return val;
+//     int i = 1;
+//     while(i <= l) {
+//         if(s[l-i] < '0' || s[l-i] > '9')
+//             return val;
 
-        int u = s[l-i] - '0';
+//         int u = s[l-i] - '0';
         
-        int j = 1;
-        int pow = 1;
-        while(j <= i-1)
-        {
-            pow *= 10;
-            j++;
-        }
+//         int j = 1;
+//         int pow = 1;
+//         while(j <= i-1)
+//         {
+//             pow *= 10;
+//             j++;
+//         }
         
-        val += u * pow;
+//         val += u * pow;
         
-        i++;
-    }
+//         i++;
+//     }
 
-    return val;
-}
+//     return val;
+// }
 
 unsigned char usart_buffer[5];
 ISR(USART0_RX_vect)
@@ -188,14 +192,66 @@ ISR(USART0_RX_vect)
     }
 }
 
-//Initialize Hall effect timer
-void Init_Hall_Timer() 
-{
-    //Set prescaler to 1
-    TCCR3B |= _BV(CS30);
+// //Initialize Hall effect timer
+// void Init_Hall_Timer() 
+// {
+//     //Set prescaler to 256
+//     TCCR3B |= _BV(CS32);
 
-    //Valeur du timer : TCNT3
-    //Voir comment clear le timer
+//     //Set CTC mode
+//     TCCR3B |= _BV(WGM32);
+// }
+
+// void Init_Hall_Interrupt()
+// {
+//     //Disable hall sensor interrupt to setup it
+//     EIMSK &= ~_BV(INT0);
+
+//     //Set interrupt at falling edge
+//     EICRA |= ISC01;
+
+//     //Enable hall sensor interrupt
+//     EIMSK |= _BV(INT0);
+// }
+
+int turnTime = 0;
+
+ISR(INT0_vect)
+{
+    turnTime = TCNT3;
+
+    //Clear le timer
+    TCNT3 = 0;
+}
+
+uint16_t leds(int deg)
+{
+    uint16_t cData = 0x0000;
+    if(hour*15 == deg){
+        cData = 0xFFFF;
+    }
+    else if(minute*6 == deg){
+        cData = 0xFF00;
+    }
+    return cData;
+}
+
+int Calc_deg(time_total,time_now)
+{
+    speed=360/time_total;
+    int deg=speed*time_now;
+    USART_Transmit('a');
+
+    return deg;
+}
+
+void LED_send(uint16_t leds){
+    char up = (leds >> 8);
+    char down = leds;
+
+    USART_Transmit('b');
+
+    SPI_MasterTransmit(up, down);
 }
 
 void main()
@@ -205,27 +261,20 @@ void main()
     USART_Init (MYUBRR);
     SPI_MasterInit();
     Init_Watch();
-    char cData = 0xFF;
-    char cData2 = 0xFF;
+    Init_Hall_Timer();
+    Init_Hall_Interrupt();
     char buffer[64];
     while (1)
     {
-        uint16_t input_d0 = PIND & 0x01; // 1 hall off - 0 hall on
-        if(input_d0 == 0){
-            //SPI_MasterTransmit(0xFF, 0xFF);
-            //USART_Transmit('a');
-        }
-        else{
-            //SPI_MasterTransmit(0x00, 0x00);
-            //USART_Transmit('b');
-        }
-        _delay_ms(1000);
-        //SPI_MasterTransmit(0xAA, 0xAA);
-        //_delay_ms(1000);
+        LED_send(leds(Calc_deg(turnTime, TCNT3)));
+        // SPI_MasterTransmit(0xFF,0x00);
+        // _delay_ms(1000);
+        // SPI_MasterTransmit(0x00, 0xFF);
+        // _delay_ms(1000);
     }
 }
 
-/*Global values
+/*Global values 
 float speed = 0;
 int mode = 0;
 int timer_value = 0;

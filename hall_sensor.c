8a7253d9a81dcb7ad12fa3533980472628f26c62
Fabien Monniot <fmonniot@ensc.fr>
2018-12-12 09:14:50 +0100

    Hall sensor interrupt

--- hall_sensor.c
+++ hall_sensor.c
@@ -148,6 +148,42 @@ ISR(USART0_RX_vect)
     // USART_Transmit(carac);
 }
 
+//Initialize Hall effect timer
+void Init_Hall_Timer() 
+{
+    //Set prescaler to 1
+    TCCR3B |= _BV(CS30);
+
+    //Set CTC mode
+    TCCR3B |= _BV(WGM32);
+}
+
+void Init_Hall_Interrupt()
+{
+    //Disable hall sensor interrupt to setup it
+    EIMSK &= ~_BV(INT0);
+
+    //Set interrupt at falling edge
+    EICRA |= ISC01;
+
+    //Enable hall sensor interrupt
+    EIMSK |= _BV(INT0);
+}
+
+int turnTime = 0;
+
+ISR(INT0_vect)
+{
+    turnTime = TCNT3;
+
+    //Clear le timer
+    TCNT3 = 0;
+
+    char buffer[64];
+    sprintf(buffer, "turn:%d", turnTime);
+    USART_puts(buffer);
+}
+
 void main()
 {
     // Active et allume la broche PB5 (led)

f64a682f8ce8fb88291eae8f30ca13a3b4cb8d90
ZdTmasar <thomas.saliba@enseirb-matmeca.fr>
2018-12-18 20:01:45 +0100

    Digital working

--- main.c
+++ main.c
@@ -5,19 +5,15 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 //#include <stdlib.h>
-#include "USART.c"
-#include "hall_sensor.c"
-#include "SPI.c"
-#include "watch.c"
 
 #define FOSC F_CPU// Clock Speed
 #define BAUD 38400
 #define MYUBRR FOSC/8/BAUD-1
 
-//int hour = 0;
-//int minute = 0;
+int hour = 0;
+int minute = 0;
 
-float speed = 0;
+//float speed = 0;
 int mode = 0;
 int timer_value = 0;
 
@@ -25,25 +21,18 @@ ISR(TIMER1_COMPA_vect)
 {
     minute++;
 
-    if(minute >= 60) {
-        minute = 0;
-        hour++;
-    }
 
-    if(hour >= 24) {
-        hour = 0;
-    }
 }
 
-// void USART_Init(unsigned int ubbr)
-// {
-//     UBRR0H = (unsigned char)(ubbr>>8); //Baudrate at 38400
-//     UBRR0L = (unsigned char)(ubbr);
+void USART_Init(unsigned int ubbr)
+{
+    UBRR0H = (unsigned char)(ubbr>>8); //Baudrate at 38400
+    UBRR0L = (unsigned char)(ubbr);
 
-//     UCSR0A = (_BV(U2X0)); //double speed
-//     UCSR0B = (_BV(RXEN0)) | (_BV(TXEN0)) | (_BV(RXCIE0)); //enable RX and Tx and interrupt on reception
-//     UCSR0C = (3 << (UCSZ00));                             //8bit char
-// }
+    UCSR0A = (_BV(U2X0)); //double speed
+    UCSR0B = (_BV(RXEN0)) | (_BV(TXEN0)) | (_BV(RXCIE0)); //enable RX and Tx and interrupt on reception
+    UCSR0C = (3 << (UCSZ00));                             //8bit char
+}
 
 
 void Init_Interrupt()
@@ -51,139 +40,151 @@ void Init_Interrupt()
     sei();
 }
 
-// void SPI_MasterInit()
-// {    
-//     /* Set MOSI and SCK output, all others input */
-//     DDRB |= (0 << DDB0) | (1 << DDB2) | (1 << DDB1) ;
-//     /* Enable SPI, Master, TODO: set clock rate fck/4 */
-//     SPCR |= (1 << SPE) | (1 << MSTR);
-//     /*Set PE4(OE), PE5 (LE)*/
-//     DDRE |= (1 << DDE4) | (1 << DDE5);
+void SPI_MasterInit()
+{    
+    /* Set MOSI and SCK output, all others input */
+    DDRB |= (1 << DDB0) | (1 << DDB2) | (1 << DDB1) ;
+    /* Enable SPI, Master, TODO: set clock rate fck/4 */
+    SPCR |= (1 << SPE) | (1 << MSTR);
+    /*Set PE4(OE), PE5 (LE)*/
+    DDRE |= (1 << DDE4) | (1 << DDE5);
 
-//     PORTB &= ~(1<<PB0);
-//     PORTE &= ~(1 << PORTE4); // OE
-//     // PORTE = PORTE | _BV(PE4) | _BV(PE5);
-// }
+    PORTB &= ~(1<<PB0);
+    PORTE &= ~(1 << PORTE4); // OE
+    // PORTE = PORTE | _BV(PE4) | _BV(PE5);
+}
 
-// void Init_Watch() 
-// {
-//     //Set prescaler to 1024
-//     TCCR1B |= _BV(CS12) |_BV(CS10);
+void USART_Transmit(unsigned char data)
+{
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
+    while ((UCSR0A & (1 << TXC0)) == 0x00);
+}
 
-//     //Set CTC mode
-//     TCCR1B |= _BV(WGM12);
+void USART_puts(unsigned char *mot){
 
-//     //Set overflow to 12695
-//     OCR1AH = 0x31;
-//     OCR1AL = 0x97;
+    //sprintf(buffer, "x=%u\r\n", TCNT1);
+    int k = 0;
+    while(mot[k] != '\0'){
+        USART_Transmit(mot[k]);
+        k++;
+    } 
+}
 
-//     //Set interrupt on compare match
-//     TIMSK |= _BV(OCIE1A);
-// }
+void SPI_MasterTransmit(char cData, char cData2)
+{
+    /* Start transmission */
 
-// void USART_Transmit(unsigned char data)
-// {
-//     while (!(UCSR0A & (1 << UDRE0)));
-//     UDR0 = data;
-//     while ((UCSR0A & (1 << TXC0)) == 0x00);
-// }
+    PORTE &= ~(1 << PORTE4); // OE
 
-// void USART_puts(unsigned char *mot){
+    PORTB &= ~(1<<PB0);
+    SPDR = cData;
 
-//     //sprintf(buffer, "x=%u\r\n", TCNT1);
-//     int k = 0;
-//     while(mot[k] != '\0'){
-//         USART_Transmit(mot[k]);
-//         k++;
-//     } 
-// }
+    /* Wait for transmission complete */
+    while (!(SPSR & (1 << SPIF)));
+    /* Start transmission */
+    PORTB |= (1<<PB0);
+    _delay_us(20);
+    PORTB &= ~(1<<PB0);
+    SPDR = cData2;
+    /* Wait for transmission complete */
+    while (!(SPSR & (1 << SPIF)));
 
-// void SPI_MasterTransmit(char cData, char cData2)
-// {
-//     /* Start transmission */
+    PORTB |= (1<<PB0);
 
-//     PORTE &= ~(1 << PORTE4); // OE
+    /* Latch and Output enable terminal*/
+    PORTE |= (1 << PORTE5); // latch
+    PORTE &= ~(1 << PORTE5);
 
-//     PORTB &= ~(1<<PB0);
-//     SPDR = cData;
+    PORTE |= (1 << PORTE4);
+    PORTE &= ~(1 << PORTE4); // OE
 
-//     /* Wait for transmission complete */
-//     while (!(SPSR & (1 << SPIF)));
-//     /* Start transmission */
-//     PORTB |= (1<<PB0);
-//     _delay_us(20);
-//     PORTB &= ~(1<<PB0);
-//     SPDR = cData2;
-//     /* Wait for transmission complete */
-//     while (!(SPSR & (1 << SPIF)));
-
-//     PORTB |= (1<<PB0);
-
-//     /* Latch and Output enable terminal*/
-//     PORTE |= (1 << PORTE5); // latch
-//     PORTE &= ~(1 << PORTE5);
-
-//     PORTE |= (1 << PORTE4);
-//     PORTE &= ~(1 << PORTE4); // OE
-
-// }
+}
 
 
-// int atoi(char* s, int l)
-// {
-//     int val = 0;
+void Init_Watch() 
+{
+    //Set prescaler to 1024
+    TCCR1B |= _BV(CS12) | _BV(CS10);
 
-//     int i = 1;
-//     while(i <= l) {
-//         if(s[l-i] < '0' || s[l-i] > '9')
-//             return val;
+    //Set CTC mode
+    TCCR1B |= _BV(WGM12);
 
-//         int u = s[l-i] - '0';
+    //Set overflow to 12695
+    OCR1AH = 0x31;
+    OCR1AL = 0x97;
+
+    //Set interrupt on compare match
+    TIMSK |= _BV(OCIE1A);
+}
+
+//Convert char array to int
+int atoi(char* s, int l)
+{
+    int val = 0;
+
+    int i = 1;
+    while(i <= l) {
+        if(s[l-i] < '0' || s[l-i] > '9')
+            return val;
+
+        int u = s[l-i] - '0';
         
-//         int j = 1;
-//         int pow = 1;
-//         while(j <= i-1)
-//         {
-//             pow *= 10;
-//             j++;
-//         }
+        int j = 1;
+        int pow = 1;
+        while(j <= i-1)
+        {
+            pow *= 10;
+            j++;
+        }
         
-//         val += u * pow;
+        val += u * pow;
         
-//         i++;
+        i++;
+    }
+
+    return val;
+}
+
+//Increment time
+// ISR(TIMER1_COMPA_vect) 
+// {
+//     minute++;
+
+//     if(minute >= 60) {
+//         minute = 0;
+//         hour++;
 //     }
 
-//     return val;
+//     if(hour >= 24) {
+//         hour = 0;
+//     }
 // }
 
-unsigned char usart_buffer[5];
-ISR(USART0_RX_vect)
+//Displays time when 'h' received from USART
+void displayTime()
 {
-    unsigned char carac = UDR0;
+    char buffer[64];
+    sprintf(buffer, "%u:%u", hour, minute);
+    USART_puts(buffer);
+}
 
-    if(carac == 'h') {
-        char buffer[64];
-        sprintf(buffer, "%u:%u", hour, minute);
-        USART_puts(buffer);
-    }
-
+//Changes time when received through USART
+unsigned char usart_buffer[5];
+void changeTime(unsigned char carac)
+{
     //Fill USART buffer 
-
-    int i = sizeof(usart_buffer)/sizeof(usart_buffer[0]) - 1;
-    while(i >= 0){
-        usart_buffer[i+1] = usart_buffer[i];
-        i--;
+    int i = 1;
+    int l = sizeof(usart_buffer)/sizeof(usart_buffer[0]);
+    while(i < l){
+        usart_buffer[i-1] = usart_buffer[i];
+        i++;
     }
 
-    usart_buffer[0] = carac;
+    usart_buffer[l-1] = carac;
 
-    if(usart_buffer[0] =='m'){
-        char temp = usart_buffer[1];
-        mode = atoi(usart_buffer, 1);
-    }
-    //If it a time, save it
-    else if(usart_buffer[2] == ':') { // Changement d'heure avant d'avoir l'info. Mettre un flage et faire le changement après
-        USART_Transmit('a');
+    //If it's a time, save it
+    if(usart_buffer[2] == ':') {
         char h[2] = {usart_buffer[0], usart_buffer[1]};
         char m[2] = {usart_buffer[3], usart_buffer[4]};
 
@@ -192,66 +193,410 @@ ISR(USART0_RX_vect)
     }
 }
 
-// //Initialize Hall effect timer
-// void Init_Hall_Timer() 
-// {
-//     //Set prescaler to 256
-//     TCCR3B |= _BV(CS32);
+ISR(USART0_RX_vect)
+{
+    unsigned char carac = UDR0;
 
-//     //Set CTC mode
-//     TCCR3B |= _BV(WGM32);
-// }
+    if(carac == 'h') {
+        displayTime();
+    }
+    else{
+        changeTime(carac);
+    }
+}
 
-// void Init_Hall_Interrupt()
-// {
-//     //Disable hall sensor interrupt to setup it
-//     EIMSK &= ~_BV(INT0);
+//Initialize Hall effect timer
+void Init_Hall_Timer() 
+{
+    //Set prescaler to 256
+    TCCR3B |= _BV(CS32);
 
-//     //Set interrupt at falling edge
-//     EICRA |= ISC01;
+    //Set CTC mode
+    TCCR3B |= _BV(WGM32);
+}
 
-//     //Enable hall sensor interrupt
-//     EIMSK |= _BV(INT0);
-// }
+void Init_Hall_Interrupt()
+{
+    //Disable hall sensor interrupt to setup it
+    EIMSK &= ~_BV(INT0);
 
-int turnTime = 0;
+    //Set interrupt at falling edge
+    EICRA |= ISC01;
+
+    //Enable hall sensor interrupt
+    EIMSK |= _BV(INT0);
+}
+
+volatile uint16_t turnTime = 0;
+volatile uint16_t TabturnTime[5] = {0,0,0,0,0};
+volatile int i = 0;
+
+volatile int tour = 0;
 
 ISR(INT0_vect)
 {
-    turnTime = TCNT3;
-
-    //Clear le timer
+    uint16_t temp = TCNT3;
     TCNT3 = 0;
+    // i++;
+    // if(i%2 == 0){
+    //     SPI_MasterTransmit(0x00,0xFF);
+    // }
+    // else{
+    //     SPI_MasterTransmit(0xFF,0x00);
+    // }
+
+    if((temp >=2000) && (temp <= 7000)){
+        //TCNT3 = 0;
+        turnTime = temp;
+        // char buffer[64];
+        // sprintf(buffer, "%d\n\r", temp);
+        // //sprintf(buffer, "\n speed:%f \n turnTime:%d time_now: %d", speed, turnTime, time_now);
+        // USART_puts(buffer); 
+    }
+    // if(temp >= 7000){
+    //     TCNT3 = 0;
+    //     turnTime = 4600;
+    // }
+    // char buffer[64];
+    // sprintf(buffer, "%d\n\r", temp);
+    // //sprintf(buffer, "\n speed:%f \n turnTime:%d time_now: %d", speed, turnTime, time_now);
+    // USART_puts(buffer); 
+    tour = 0;
 }
 
-uint16_t leds(int deg)
+uint16_t leds(uint32_t deg)
 {
     uint16_t cData = 0x0000;
-    if(hour*15 == deg){
-        cData = 0xFFFF;
+    if((minute*(turnTime/60) >= deg-10)&&(minute*(turnTime/60) <= deg+10)){
+        cData = 0x00FF;
     }
-    else if(minute*6 == deg){
-        cData = 0xFF00;
+    if(((hour%12)*(turnTime/12)  >= deg-10) && ((hour%12)*(turnTime/12) <= deg+10)){
+        cData = 0x000F;
     }
+    
+    
     return cData;
 }
 
-int Calc_deg(time_total,time_now)
+int Calc_deg(time_now)
 {
-    speed=360/time_total;
-    int deg=speed*time_now;
-    USART_Transmit('a');
+    uint32_t deg=time_now;
+    // if(time_now >=4600){
+    //     TCNT3 = 0;
+    // }
 
+    //deg = (deg + turnTime/2)%turnTime;
+    //char buffer[64];
+    // sprintf(buffer, "%d\n\r", time_now);
+    // //sprintf(buffer, "\n speed:%f \n turnTime:%d time_now: %d", speed, turnTime, time_now);
+    // USART_puts(buffer);
     return deg;
 }
 
 void LED_send(uint16_t leds){
     char up = (leds >> 8);
     char down = leds;
-
-    USART_Transmit('b');
-
     SPI_MasterTransmit(up, down);
+}
+
+#include <stdbool.h>
+
+bool digits[10][14][7] = {
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 1, 0, 0, 0},
+        {0, 1, 0, 1, 0, 0, 0},
+        {1, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+    },
+    {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+    }
+};
+
+bool colon[14][2] = {
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {1, 1},
+    {1, 1},
+    {0, 0},
+    {0, 0},
+    {1, 1},
+    {1, 1},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+};
+//int hash = 60;
+
+bool ledStates[60][16];
+
+void reinitArrayBool2() {
+    volatile int i = 0;
+    volatile int li = sizeof(ledStates[0])/sizeof(ledStates[0][0]);
+    while(i < li) {
+        volatile int j = 0;
+        volatile int lj = sizeof(ledStates)/sizeof(ledStates[0][0]);
+        while(j < lj) {
+            ledStates[i][j] = 0;
+            j++;
+        }
+        i++;
+    }
+}
+
+int digitToLedStates(int digit, int a) {
+    volatile int i = 0;
+    //volatile int li = sizeof(digits[digit][0])/sizeof(digits[digit][0][0]);
+    //printf("li : %d", li);
+    volatile int li = 7;
+    volatile int j = 13;
+    //volatile int lj = sizeof(digits[digit])/sizeof(digits[digit][0][0]);
+    //printf("lj : %d", lj);
+    //volatile int lj = 14;
+    while(i < li) {
+        j = 13;
+        while(j >= 0) {
+            ledStates[a+i][13-j+2] = digits[digit][j][i];
+            j--;
+        }
+        i++;
+    }
+
+    return a+i+1;
+}
+
+int colonToLedStates(int a) {
+    volatile int i = 0;
+    volatile int li = 2;
+    volatile int j = 0;
+    volatile int lj = 14;
+    while(i < li) {
+        j = 0;
+        while(j < lj) {
+            ledStates[a+i][j+2] = colon[j][i];
+            j++;
+        }
+        i++;
+    }
+
+    return a+i+1;
+}
+
+
+
+void hourToCurveLed() {
+    //Prepare data
+    reinitArrayBool2();
+
+    int h1 = hour/10;
+    int h2 = hour - h1*10;
+    int m1 = minute/10;
+    int m2 = minute - m1*10;
+
+    int i = 13;
+
+    i = digitToLedStates(h1, i);
+    i = digitToLedStates(h2, i);
+    i = colonToLedStates(i);
+    i = digitToLedStates(m1, i);
+    i = digitToLedStates(m2, i);
+}
+
+volatile int lastHour = 0;
+volatile int lastMinute = 0;
+
+volatile char char1 = 0x0000;
+volatile char char2 = 0x0000;
+
+void displayCurveTime() {
+    if(lastMinute != minute || lastHour != hour) {
+        hourToCurveLed();
+        lastHour = hour;
+        lastMinute = minute;
+    }
+
+    if(TCNT3 >= (turnTime/60)*(tour+1)) {
+        tour += 1;
+
+        char1 = 0x0000;
+        char2 = 0x0000;
+
+        volatile int j = 0;
+        while(j < 16) {
+            if(j < 8 && ledStates[tour][j])
+                char1 |= 1 << (j%8);
+            else if(j >= 8 && ledStates[tour][j])
+                char2 |= 1 << (j%8);
+            j++;
+        }
+    }
+    
+    SPI_MasterTransmit(char2, char1);
+}
+
+void watch_tick(){
+    if(minute >= 60) {
+        minute = 0;
+        hour++;
+    }
+
+    if(hour >= 24) {
+        hour = 0;
+    }
 }
 
 void main()
@@ -263,14 +608,21 @@ void main()
     Init_Watch();
     Init_Hall_Timer();
     Init_Hall_Interrupt();
+
+
+    hourToCurveLed();
+
     char buffer[64];
     while (1)
     {
-        LED_send(leds(Calc_deg(turnTime, TCNT3)));
+        // USART_puts(" a");
+        //LED_send(leds(Calc_deg(TCNT3)));
+        watch_tick();
+        displayCurveTime();
+        // USART_puts(" end\n");
         // SPI_MasterTransmit(0xFF,0x00);
         // _delay_ms(1000);
-        // SPI_MasterTransmit(0x00, 0xFF);
-        // _delay_ms(1000);
+        //SPI_MasterTransmit(0x00, 0xFF);
     }
 }
 
